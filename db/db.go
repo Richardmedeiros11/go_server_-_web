082c08686953f98c69378d8a8ea658ecf5177b16
Richard_Medeiros <113040321+Richardmedeiros11@users.noreply.github.com>
2024-03-21 10:09:48 -0300

    Add files via upload

---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func ConectaComBancoDeDados() *sql.DB {
+	conexao := "user=richard dbname=alura_loja password=richard11072008 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import "github.com/Richard_Medeiros/proj1/db"
+
+type Produtos struct {
+	Id         int
+	Nome       string
+	Descrição  string
+	Preco      float64
+	Quantidade int
+}
+
+func BuscaProdutos() []Produtos {
+	db := db.ConectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	p := Produtos{}
+	produtos := []Produtos{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descrição string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descrição, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descrição = descrição
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+	defer db.Close()
+	return produtos
+}
